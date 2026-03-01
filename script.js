a3b4e846ba345c1de2759ad7025f1d436ab1c1b3
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-01 01:05:29 -0800

    Update js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showMessage() {
-  alert("Keep going Nikita! You're becoming a great developer 🚀");
+  alert("Keep going Nikita! You're becoming a great developer one day🚀");
 
   document.body.style.background =
     "linear-gradient(to right, #a18cd1, #fbc2eb)";
